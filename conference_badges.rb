33e9816d07bab5ce3e5c69f005fc395fdd9b21c8
Alexis Torosina <alexistorosina1@gmail.com>
2022-10-31 19:18:32 -0400

    commit tree d2be421a224d4a8ed9bf81eba8c0b5e65ea1666c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+    speakers.map do |speaker|
+        "Hello, my name is #{speaker}."
+    end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room_num|
+        "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |badge|
+        puts badge
+    end
+    assign_rooms(speakers).each do |rooms|
+        puts rooms
+    end
+end
